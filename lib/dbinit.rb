ad3b39ce1156ab413754752026885d982df5d554
yasuo kominami <ykominami@gmail.com>
2015-07-09 21:12:33 +0900

    init 9

--- lib/dbinit.rb
+++ lib/dbinit.rb
@@ -2,7 +2,7 @@
 
 module Arxutils
   class Dbinit
-    def Dbinit.init( config_path , log_fname )
+    def Dbinit.init( config_path: 'config/sqlite3.yaml' , log_fname: 'db/database.log' )
       dbconfig = YAML.load( File.read( config_path ) )
       ActiveRecord::Base.establish_connection(dbconfig[ENV['ENV']])
       ActiveRecord::Base.logger = Logger.new( log_fname )
