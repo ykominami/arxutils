25060f56d94d3c30d1417eca54769169db957e9c
yasuo kominami <ykominami@gmail.com>
2016-05-15 19:06:20 +0900

    add update_integer, modify end_count_id -> count_id

--- lib/arxutils/arx.rb
+++ lib/arxutils/arx.rb
@@ -25,5 +25,17 @@ module Arxutils
       erb = ERB.new(contents)
       erb.result(binding)
     end
+
+    def update_integer( model , hs )
+      value_hs = hs.reduce({}){ |hsx,item|
+        val = model.send(item[0])
+        if val != nil and item[1] != nil and val  < item[1]
+          hsx[ item[0] ] = item[1]
+        end
+        hsx
+      }
+      model.update(value_hs) if value_hs.size > 0
+    end
+
   end
 end
--- lib/arxutils/version.rb
+++ lib/arxutils/version.rb
@@ -1,3 +1,3 @@
 module Arxutils
-  VERSION = "0.1.32"
+  VERSION = "0.1.33"
 end
