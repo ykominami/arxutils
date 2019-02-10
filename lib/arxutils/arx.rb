b52ce2fdeca79ad3b009262839e6cf7e6ba6fd5b
yasuo kominami <ykominami@gmail.com>
2019-02-10 18:23:19 +0900

    fixes #54 add rdoc comment

--- lib/arxutils/arx.rb
+++ lib/arxutils/arx.rb
@@ -2,10 +2,15 @@
 require 'erb'
 
 module Arxutils
+  # スキーマ設定に基づき、テンプレートから変換する
   class Arx
+    # スキーマ設定配列を、テンプレートで参照可能になるように展開する
     def initialize( data , fname )
+      # テンプレートファイルへのパス
       @fname = fname
-      # 以下のものの配列
+
+      # スキーマ設定配列
+      # スキーマ設定とは以下をキーにもつハッシュである
       # :flist
       # :classname
       # :classname_downcase
@@ -15,6 +20,7 @@ module Arxutils
       # :relation
       @data = data
 
+      # スキーマ設定の:itemsの値を展開後格納するためのStructクラス
       @@field ||= Struct.new("Field" , :name, :type, :null ) 
 
       if @data[:items]
@@ -24,6 +30,7 @@ module Arxutils
       end
     end
 
+    # テンプレートファイルを元にした変換結果を返す
     def create
       contents = File.open( @fname ).read
       erb = ERB.new(contents)
--- lib/arxutils/version.rb
+++ lib/arxutils/version.rb
@@ -1,3 +1,4 @@
 module Arxutils
+  # バージョン番号
   VERSION = "0.1.37"
 end
